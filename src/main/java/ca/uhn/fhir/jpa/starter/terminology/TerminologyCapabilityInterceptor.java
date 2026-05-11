dbe75f9fe3ef8df37f0499b5cef864ece2e849f7
Joel Bach <6702422+joel-bach@users.noreply.github.com>
2026-05-11 09:36:34 +0200

    update terminologycapabilities

--- src/main/java/ca/uhn/fhir/jpa/starter/terminology/TerminologyCapabilityInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/terminology/TerminologyCapabilityInterceptor.java
@@ -6,8 +6,11 @@ import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.ResponseDetails;
 import org.hl7.fhir.r4.model.CapabilityStatement;
+import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.TerminologyCapabilities;
 
+import java.sql.Date;
+import java.time.Instant;
 import java.util.Arrays;
 
 @Interceptor
@@ -20,6 +23,20 @@ public class TerminologyCapabilityInterceptor {
 			capabilities.setVersion("1.0.0");
 			capabilities.setName("TerminologyCapabilities");
 			capabilities.setTitle("Terminology capabilities");
+			capabilities.setStatus(Enumerations.PublicationStatus.ACTIVE);
+			capabilities.setDate(Date.from(Instant.parse("2026-05-11T07:30:00Z")));
+			capabilities.setExpansion(new TerminologyCapabilities.TerminologyCapabilitiesExpansionComponent()
+				.addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("count"))
+				.addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("displayLanguage"))
+				.addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("excludeNested"))
+				.addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("offset"))
+				.addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("tx-resource"))
+				// activeOnly and includeDesignations are accepted by the server
+				// but are not applied (they do not have any effect currently).
+				// Therefore, they are excluded here.
+				// .addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("activeOnly"))
+				// .addParameter(new TerminologyCapabilities.TerminologyCapabilitiesExpansionParameterComponent().setName("includeDesignations"))
+			);
 			theResponseDetails.setResponseResource(capabilities);
 		}
 	}
